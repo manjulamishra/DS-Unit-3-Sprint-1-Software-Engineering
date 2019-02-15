65936401e9c84bfb2393bc402784d70f1294e5f7
Manjula Mishra <manjulamishra@users.noreply.github.com>
2019-02-15 10:56:56 -0800

    added two more tests

--- acme_test.py
+++ acme_test.py
@@ -12,6 +12,16 @@ class AcmeProductTests(unittest.TestCase):
         prod = Product('Test Product')
         self.assertEqual(prod.price, 10)
 
+    def test_default_product_price(self):
+        """Test default product price being 10."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.weight, 20)
+
+    def test_default_product_price(self):
+        """Test default product price being 10."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.flammability, 0.5)    
+
 
 if __name__ == '__main__':
     unittest.main()
